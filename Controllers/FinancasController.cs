a73d8cd00c93c42080a2d92a99f74bc4272afea8
Kleber777 <klebermirandalima@gmail.com>
2023-06-11 02:38:00 -0300

    Salvando as publicações

---
+++ Controllers/FinancasController.cs
@@ -0,0 +1,28 @@
+// using Microsoft.AspNetCore.Mvc;
+// using Application;
+
+// namespace Api.Controllers
+// {
+//     [ApiController]
+//     [Route("[controller]")]
+//     public class DetailController : ControllerBase
+//     {
+
+//         private readonly DetailService _detailService;
+//         public DetailController()
+//         {
+//             _detailService = new DetailService();
+//         }
+
+//         [HttpGet]
+//         public string Get()
+//         {
+//             return "Success";
+//         }
+
+//         [HttpGet("UniqueDetail/{id}")]
+//         public DetailDto Detail(int id) =>
+//             _detailService.GetAllDetails(id);
+
+//     }
+// }
